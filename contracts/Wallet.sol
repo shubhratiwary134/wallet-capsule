972c2e97b411dba04227efceaf7035cbeb6f6552
shubhra tiwary <shubhratiwary5@gmail.com>
2025-06-25 20:00:12 +0530

    basic deposit function to store the WEI in the mapping trackEth .

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.28;
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract Wallet {
     address public owner;
+    mapping(address => uint) public trackEth;
     constructor() {
         owner = msg.sender;
+    }
+    function deposit() external payable {
+        require(msg.value > 0, "Caller of the function must send some WEI.");
+        trackEth[msg.sender] += msg.value;
     }
 }
