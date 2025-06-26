ddb3d68cc02c4b4934a7cafca02c044a110ec3a3
shubhra tiwary <shubhratiwary5@gmail.com>
2025-06-27 04:14:13 +0530

    shifting from one user one safe logic to one user multiple safe logics . receive function remains

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -6,36 +6,53 @@ import "hardhat/console.sol";
 
 contract Wallet {
     address public owner;
-    mapping(address => uint) public trackEth;
+    struct Safe {
+        uint amount;
+        uint unlockTime;
+    }
+    uint public maxLimit = 5 ether;
+    mapping(address => Safe[]) public userSafes;
     event Deposited(address indexed sender, uint amount, uint timestamp);
     event Withdrawn(address indexed sender, uint amount, uint timestamp);
     constructor() {
         owner = msg.sender;
     }
     receive() external payable {
-        trackEth[msg.sender] = msg.value;
+        require(
+            msg.value > 0 && msg.value <= maxLimit,
+            "Higher than the limit of none ether sent."
+        );
         emit Deposited(msg.sender, msg.value, block.timestamp);
     }
     fallback() external payable {
         revert("Invalid function call,REVERTED");
     }
-    function deposit() external payable {
-        require(msg.value > 0, "Caller of the function must send some WEI.");
-        trackEth[msg.sender] += msg.value;
+    function deposit(uint lockTime) external payable {
+        require(
+            msg.value > 0 && msg.value <= maxLimit,
+            "Higher than the limit of none ether sent."
+        );
+        userSafes[msg.sender].push(Safe(msg.value, block.timestamp + lockTime));
         emit Deposited(msg.sender, msg.value, block.timestamp);
     }
-    function withdraw(uint amount) external {
+    function withdraw(uint amount, uint safeIndex) external {
+        Safe storage safe = userSafes[msg.sender][safeIndex];
         require(
-            amount <= trackEth[msg.sender],
-            "The amount asked for the withdraw is more than the deposited"
+            safeIndex < userSafes[msg.sender].length,
+            "The Safe does not exist."
         );
-        trackEth[msg.sender] -= amount;
+        require(
+            amount <= safe.amount,
+            "The amount asked for the withdraw is more than the deposited."
+        );
+        require(block.timestamp > safe.unlockTime, "The safe is still locked");
+        safe.amount = safe.amount - amount;
 
         (bool success, ) = payable(msg.sender).call{value: amount}("");
         require(success, "The transaction was not complete");
         emit Withdrawn(msg.sender, amount, block.timestamp);
     }
-    function displayBalance() external view returns (uint) {
-        return trackEth[msg.sender];
+    function getSafes() external view returns (Safe[] memory) {
+        return userSafes[msg.sender];
     }
 }
